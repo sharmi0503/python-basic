d3b990146b08240dd0febb726f02f1c47a9cf6bc
sharmi0503 <143270315+sharmi0503@users.noreply.github.com>
2023-10-25 14:51:09 +0530

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,34 @@
+# Python Program to calculate the square root
+#Example: For positive numbers
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+#Output
+
+The square root of 8.000 is 2.828
+
+#Source code: For real or complex numbers
+
+# Find square root of real or complex numbers
+# Importing the complex math module
+import cmath
+
+num = 1+2j
+
+# To take input from the user
+#num = eval(input('Enter a number: '))
+
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
+#Output
+
+The square root of (1+2j) is 1.272+0.786j
+
